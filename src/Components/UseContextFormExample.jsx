c86c7a6b3bc74fff464042245b8e915da3b2bca7
Agalya-10 <aagalya360@gmail.com>
2025-01-23 16:29:45 +0530

    Today Updates

--- src/Components/UseContextFormExample.jsx
+++ src/Components/UseContextFormExample.jsx
@@ -1,192 +1,35 @@
-import React, { createContext, useContext, useState } from "react";
-const LocationContext = createContext();
-
-const LocationProvider = ({ children }) => {
-  
-  const [location, setLocation] = useState({
-    country: "",
-    state: "",
-    city: "",
-  });
-  const countries = {
-    India: {
-      TamilNadu: ["Chennai", "Madurai", "Thanjavur"],
-      Kerala: ["Kochi", "Trivandrum"],
-    },
-    USA: {
-      California: ["Los Angeles", "San Francisco"],
-      Texas: ["Houston", "Dallas"],
-    },
-  };
-
-  //  country 
-  const handleCountryChange = (country) => {
-    setLocation({ country, state: "", city: "" });
-  };
-
-// state
-  const handleStateChange = (state) => {
-    setLocation((prev) => ({ ...prev, state, city: "" }));
-  };
-
-  //  city 
-  const handleCityChange = (city) => {
-    setLocation((prev) => ({ ...prev, city }));
-  };
-
-  return (
-    <LocationContext.Provider
-      value={{
-        location,
-        countries,
-        handleCountryChange,
-        handleStateChange,
-        handleCityChange,
-      }}
-    >
-      {children}
-    </LocationContext.Provider>
-  );
-};
-
-const LocationForm = () => {
-  const {
-    location,
-    countries,
-    handleCountryChange,
-    handleStateChange,
-    handleCityChange,
-  } = useContext(LocationContext);
-
-  const states = location.country ? Object.keys(countries[location.country]) : [];
-  const cities = location.state ? countries[location.country][location.state] : [];
-
-  return (
-    <div>
-      <form  className="form3" style={{ width: '400px' }}>
-      <h3  className="head1">Location Form</h3>
-<div className="label2 mt-1 " style={{ width: '100%' }}>
-      <label> Name </label>
-      <input type="text" name="name" placeholder="Name" autoComplete="off"  className="box3 p-1" style={{width:'100%'}}></input><br/>
-      </div>
-      <div className="label2 mt-1" style={{ width: '100%' }}>
-      <label> Email </label>
-      <input type="text" name="email" placeholder="Email" autoComplete="off" className="box3 p-1" style={{width:'100%'}}></input><br/>
-</div>
-        {/* Country*/}
-        <div className="label1 mt-1" style={{ width: '100%' }}>
-        <label>Country </label>
-        <select
-          className="box3 p-1" 
-          style={{width:'100%'}}
-          value={location.country}
-          onChange={(e) => handleCountryChange(e.target.value)}
-        >
-          <option value="" disabled>Select Country</option>
-          {Object.keys(countries).map((country) => (
-            <option key={country} value={country}>
-              {country}
-            </option>
-          ))}
-        </select><br/>
-
-        </div>
-
-        {/* State*/}
-        <div className="label1 mt-1" style={{ width: '100%' }}>
-        <label> State </label>
-        <select
-          className="box3 p-1"
-          style={{width:'100%'}}
-          value={location.state}
-          onChange={(e) => handleStateChange(e.target.value)}
-          disabled={!location.country}
-        >
-          <option value="" disabled>Select State</option>
-          {states.map((state) => (
-            <option key={state} value={state}>
-              {state}
-            </option>
-          ))}
-        </select><br/>
-</div>
-       {/* city */}
-       <div className="label1 mt-1" style={{ width: '100%' }}>
-        <label> City </label>
-        <select
-          className="box3 p-1" 
-          style={{width:'100%'}}
-          value={location.city}
-          onChange={(e) => handleCityChange(e.target.value)}
-          disabled={!location.state}
-        >
-          <option value="" disabled>Select City</option>
-          {cities.map((city) => (
-            <option key={city} value={city}>
-              {city}
-            </option>
-          ))}
-        </select>
-        </div>
-        <button className="button2" type="submit" style={{ width: '100%' }}>Register
-            </button>
-            <p className="para2">
-              Already have an Account?
-              <a
-                href=""
-                className="link"
-                 style={{color:' rgb(235, 32, 59)'}}
-              >
-                Login
-              </a>
-            </p>
-      </form>
-    </div>
-  );
-};
-
-// Main App
-const UseContextFormExample = () => {
-  return (
-    <LocationProvider>
-      <LocationForm />
-    </LocationProvider>
-  );
-};
-
-export default UseContextFormExample;
-
-
-
-
-
 import React, { createContext, useContext, useReducer, useEffect } from "react";
-
 const LocationContext = createContext();
-
-// Initial state
-const initialState = JSON.parse(localStorage.getItem("location")) || {
-  country: "",
-  state: "",
-  city: "",
-};
-
-// Reducer function
 const locationReducer = (state, action) => {
   switch (action.type) {
     case "SET_COUNTRY":
-      return { country: action.payload, state: "", city: "" };
+      return { ...state, country: action.payload, state: "", city: "" };
     case "SET_STATE":
       return { ...state, state: action.payload, city: "" };
     case "SET_CITY":
       return { ...state, city: action.payload };
+    case "SET_NAME":
+      return { ...state, name: action.payload };
+    case "SET_EMAIL":
+      return { ...state, email: action.payload };
+    case "RESET":
+      localStorage.removeItem("location");
+      return initialLocation;
     default:
       return state;
   }
 };
 
+const initialLocation = {
+  country: "",
+  state: "",
+  city: "",
+  name: "",
+  email: "",
+};
+
 const LocationProvider = ({ children }) => {
-  const [location, dispatch] = useReducer(locationReducer, initialState);
+  const [location, dispatch] = useReducer(locationReducer, initialLocation);
 
   const countries = {
     India: {
@@ -198,32 +41,17 @@ const LocationProvider = ({ children }) => {
       Texas: ["Houston", "Dallas"],
     },
   };
-
-  // Sync state with localStorage
   useEffect(() => {
+    console.log("Updated Location:", location);
     localStorage.setItem("location", JSON.stringify(location));
   }, [location]);
-
-  const handleCountryChange = (country) => {
-    dispatch({ type: "SET_COUNTRY", payload: country });
-  };
-
-  const handleStateChange = (state) => {
-    dispatch({ type: "SET_STATE", payload: state });
-  };
-
-  const handleCityChange = (city) => {
-    dispatch({ type: "SET_CITY", payload: city });
-  };
 
   return (
     <LocationContext.Provider
       value={{
         location,
         countries,
-        handleCountryChange,
-        handleStateChange,
-        handleCityChange,
+        dispatch,
       }}
     >
       {children}
@@ -232,23 +60,28 @@ const LocationProvider = ({ children }) => {
 };
 
 const LocationForm = () => {
-  const {
-    location,
-    countries,
-    handleCountryChange,
-    handleStateChange,
-    handleCityChange,
-  } = useContext(LocationContext);
+  const { location, countries, dispatch } = useContext(LocationContext);
 
   const states = location.country ? Object.keys(countries[location.country]) : [];
   const cities = location.state ? countries[location.country][location.state] : [];
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    localStorage.setItem("submittedLocation", JSON.stringify(location));
+
+    console.log("Form Submitted:", location);
+    alert("Form Submitted Successfully!");
+
+    dispatch({ type: "RESET" });
+  };
+
   return (
     <div>
-      <form className="form3" style={{ width: "400px" }}>
+      <form className="form3" style={{ width: "400px" }} onSubmit={handleSubmit}>
         <h3 className="head1">Location Form</h3>
         <div className="label2 mt-1" style={{ width: "100%" }}>
-          <label> Name </label>
+          <label>Name</label>
           <input
             type="text"
             name="name"
@@ -256,11 +89,13 @@ const LocationForm = () => {
             autoComplete="off"
             className="box3 p-1"
             style={{ width: "100%" }}
+            value={location.name}
+            onChange={(e) => dispatch({ type: "SET_NAME", payload: e.target.value })}
           />
           <br />
         </div>
         <div className="label2 mt-1" style={{ width: "100%" }}>
-          <label> Email </label>
+          <label>Email</label>
           <input
             type="text"
             name="email"
@@ -268,17 +103,18 @@ const LocationForm = () => {
             autoComplete="off"
             className="box3 p-1"
             style={{ width: "100%" }}
+            value={location.email}
+            onChange={(e) => dispatch({ type: "SET_EMAIL", payload: e.target.value })}
           />
           <br />
         </div>
-        {/* Country */}
         <div className="label1 mt-1" style={{ width: "100%" }}>
-          <label>Country </label>
+          <label>Country</label>
           <select
             className="box3 p-1"
             style={{ width: "100%" }}
             value={location.country}
-            onChange={(e) => handleCountryChange(e.target.value)}
+            onChange={(e) => dispatch({ type: "SET_COUNTRY", payload: e.target.value })}
           >
             <option value="" disabled>
               Select Country
@@ -291,15 +127,13 @@ const LocationForm = () => {
           </select>
           <br />
         </div>
-
-        {/* State */}
         <div className="label1 mt-1" style={{ width: "100%" }}>
-          <label> State </label>
+          <label>State</label>
           <select
             className="box3 p-1"
             style={{ width: "100%" }}
             value={location.state}
-            onChange={(e) => handleStateChange(e.target.value)}
+            onChange={(e) => dispatch({ type: "SET_STATE", payload: e.target.value })}
             disabled={!location.country}
           >
             <option value="" disabled>
@@ -313,14 +147,13 @@ const LocationForm = () => {
           </select>
           <br />
         </div>
-        {/* City */}
         <div className="label1 mt-1" style={{ width: "100%" }}>
-          <label> City </label>
+          <label>City</label>
           <select
             className="box3 p-1"
             style={{ width: "100%" }}
             value={location.city}
-            onChange={(e) => handleCityChange(e.target.value)}
+            onChange={(e) => dispatch({ type: "SET_CITY", payload: e.target.value })}
             disabled={!location.state}
           >
             <option value="" disabled>
@@ -338,11 +171,7 @@ const LocationForm = () => {
         </button>
         <p className="para2">
           Already have an Account?
-          <a
-            href=""
-            className="link"
-            style={{ color: " rgb(235, 32, 59)" }}
-          >
+          <a href="" className="link" style={{ color: "rgb(235, 32, 59)" }}>
             Login
           </a>
         </p>
@@ -351,7 +180,7 @@ const LocationForm = () => {
   );
 };
 
-const UseContextFormExample = () => {
+const UseReducerFormExample = () => {
   return (
     <LocationProvider>
       <LocationForm />
@@ -359,4 +188,4 @@ const UseContextFormExample = () => {
   );
 };
 
-export default UseContextFormExample;
+export default UseReducerFormExample;
